26bbb9f32a2ce6ae645aed1ce0879487ff714953
Joachim <joachim.bachstatter@gmail.com>
2013-11-26 15:40:00 +0100

    moved admin pwd to config
    also made an example-config

---
+++ config-example.php
@@ -0,0 +1,23 @@
+<?php 
+
+//remove example and put in your info
+
+date_default_timezone_set('Europe/Stockholm');
+
+define('ROOT_PATH', dirname(__FILE__));
+
+
+// Database configurations
+define('DB_HOST', 'host');
+define('DB_NAME', 'dbname');
+define('DB_USER', 'dbuser');
+define('DB_PASS', 'dbpwd');
+
+
+ // Login for admin page configurations
+$username = "user";
+$password = "pwd";
+$nonsense = "supercalifragilistohoihohpialidocious";
+
+
+ ?>
