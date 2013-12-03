7dfc4a4eb499dda8ea6d3d0e972a946ad9a5f862
Joachim <joachim.bachstatter@gmail.com>
2013-12-03 15:04:50 +0100

    Lets you edit categoryId

--- public_html/admin/portfolio/edit.php
+++ public_html/admin/portfolio/edit.php
@@ -1,7 +1,7 @@
 <?php 
 	require_once '../../../application.php';
 	Authorization::checkOrRedirect();
-
+	$category = Category::all();
   	$item = PortfolioItem::find($_GET['id']);
 
 	if(isset($_POST['item'])) {
@@ -24,13 +24,24 @@
 			<h1 class="invert">Edit: <?php echo $item->title; ?></h1>
 			<a href="/admin/portfolio/" class="back">Tillbaka</a>
 			<form class="editform" action="" method="POST" enctype="multipart/form-data">
-
+				<input type="hidden" name="item[id]" value="<?php echo $item->id; ?>">
+			    
 			    <label for="title">Title</label><br>
 			    <input type="text" id="title" name="item[title]" value="<?php echo $item->title; ?>"> <br><br>
 
 				<label for="content">Content</label>
 				<textarea id="content" name="item[content]"><?php echo $item->content; ?></textarea> <br><br>
 				
+				<label for="Category">Category</label>
+				<select id="Category" name="item[categoryId]">
+						<option value="">Ingen Kategori</option>
+					<?php foreach ($category as $category): ?>
+						<option value="<?php echo $category->id ?>" <?php if ($item->categoryId == $category->id) {echo "selected";} ?> > 
+							<?php echo $category->name; ?>
+						</option>
+					<?php endforeach; ?>
+				</select> <br><br
+
 				<label for="imageName">Image</label><br>
 			    <input type="file" id="title" name="file">
 
