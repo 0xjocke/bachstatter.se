0f32367a6054ed92de8683c2e6ed13bc9802e31c
Joachim <joachim.bachstatter@gmail.com>
2013-12-05 19:48:42 +0100

    add comments

--- models/authorization.php
+++ models/authorization.php
@@ -2,7 +2,10 @@
 
 class Authorization extends BaseModel
 {
-
+  // takes in username and password
+  // search for/width the usernamne and the findUser method in User class
+  // Hash the pwd and adds the email as a salt, making it unique
+  // Is DB match set $_Session to true and return method check
   public static function authenticate($userName, $password) {
     $user = User::findUser($userName);
     $password = hash("sha256", $password.$user['userName']);
@@ -12,17 +15,20 @@ class Authorization extends BaseModel
     return self::check();
   }
 
+// returns true if session is set and equal to 'is_authenticated'
   public static function check() {
-    // om session is_authenticated är definerat och om den har ett sant värde så
-    // kommer vi returna true annars false
+  
     return (isset($_SESSION['is_authenticated']) && $_SESSION['is_authenticated']);
   }
 
+//unset the session and direct to login page
   public static function logout() {
     unset($_SESSION['is_authenticated']);
     header('Location: /admin/login.php');
     exit;
   }
+
+  // if check() not is true direct to login page and exit.
 
   public static function checkOrRedirect() {
     if (!self::check()) {
