fe6fdb05e4a635c086e0651bad34d055a01da095
Joachim <joachim.bachstatter@gmail.com>
2013-12-05 22:01:40 +0100

    add comments

--- models/user.php
+++ models/user.php
@@ -1,8 +1,11 @@
 <?php 
 class User extends BaseModel
 {
+	// sets table name
 	const TABLE_NAME = 'user';
-
+	// takes a user name as parameter (string)
+	// connect to db and finds userName
+	// fetch that row.
 	public static function findUser($userName){
 
 	$statement = self::$dbh->prepare("SELECT * FROM user WHERE userName = :userName");
@@ -10,7 +13,6 @@ class User extends BaseModel
 	$rs =  $statement->fetch();
 	return $rs;
 	}
-
 }
 
 
