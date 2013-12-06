6b0a5302266294827f1bff0be485337b1a77c70b
Joachim <joachim.bachstatter@gmail.com>
2013-12-06 20:04:57 +0100

    add email settings

--- config-example.php
+++ config-example.php
@@ -13,4 +13,14 @@ define('DB_NAME', 'dbname');
 define('DB_USER', 'dbuser');
 define('DB_PASS', 'dbpwd');
 
+// Admin
+define('ADMIN_EMAIL', 'admin email');
+define('ADMIN_NAME', 'admin name');
+
+// Smtp configuration
+define('SMTP_ADRESS', ' email server');
+define('SMTP_PORT', 'port');
+define('SMTP_ENCRYPT_PROTOCOL', 'protocol');
+define('SMTP_USER', 'email to send to');
+define('SMTP_PASS', 'email password');
  ?>
